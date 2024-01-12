74639b5062d11d4dae12551e8d104acbc8cf7db9
Joe Abell <jsphabll@gmail.com>
2024-01-12 09:15:43 +0000

    allow more urls in the lambda

--- netlify/functions/password.js
+++ netlify/functions/password.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "http://localhost:8080",
+        "Access-Control-Allow-Origin": "http://localhost:8080, https://josephabell.co.uk, https://joseph-abell.netlify.app",
         "Access-Control-Allow-Headers": "Content-Type",
       },
       body: JSON.stringify({ isPotato }),
