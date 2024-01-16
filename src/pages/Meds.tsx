c30f1b348c6afb17062b5a26ee590a45592f383c
Joseph Abell <joseph.a.abell@tescobank.com>
2024-01-16 15:03:31 +0000

    shrink meds lines

--- src/pages/Meds.tsx
+++ src/pages/Meds.tsx
@@ -3,12 +3,12 @@ import DefaultLayout from "../layouts/DefaultLayout";
 import { supabase } from '../helpers';
 
 const Row = (props: any) => {
-    const y = props.index() * 3 * 10;
+    const y = props.index() * 3 * 2;
 return (
     <g>
-        <line x1="0" y1={y + 5} x2="100%" y2={y + 5} stroke={props.row.breakfast ? 'yellow' : 'black'} stroke-width='10' />
-        <line x1="0" y1={y + 15} x2="100%" y2={y + 15} stroke={props.row.lunch ? 'blue' : 'black'} stroke-width='10' />
-        <line x1="0" y1={y + 25} x2="100%" y2={y + 25} stroke={props.row.dinner ? 'red' : 'black'} stroke-width='10' />
+        <line x1="0" y1={y + 1} x2="100%" y2={y + 1} stroke={props.row.breakfast ? 'yellow' : 'black'} stroke-width='2' />
+        <line x1="0" y1={y + 3} x2="100%" y2={y + 3} stroke={props.row.lunch ? 'blue' : 'black'} stroke-width='2' />
+        <line x1="0" y1={y + 5} x2="100%" y2={y + 5} stroke={props.row.dinner ? 'red' : 'black'} stroke-width='2' />
     </g>
 ) }
 
@@ -49,8 +49,7 @@ const HorizontalLinesSVG = () => {
                 <div class="container meds-container" style={{ position: 'absolute', 'z-index': 1, left: '50vw', 'margin-left': '-25vw', 'margin-top': '40px', 'border': '2px solid #333' }}>
                     <h1>Medicine Tracker</h1>
 
-                    <p>The idea of this tracker came from the Sunday Stone. The Sunday Stone is a stone found near coal mines in the North East of England. The stone has black and while horizontal stripes on it. Black, for times where the coal miners were working, leaving a residue of coal. White, for the limescape buildup, during times where the miners were not working. There was always an extra large patch of white, coinciding with Sundays or holidays, when the mine was closed. <a target='_blank' href='https://en.wikipedia.org/wiki/Sunday_stone'>Wiki, for more info.</a></p>
-                    <p>Each of these lines in the background show me successfully taking my diabetes medication. Yellow is for breakfast, blue is for lunch, and red is for dinner. If there are any black gaps, it shows a time where I forgot to take my meds. Depending on when you view this page, you'll probably see some black space near the top of this page, which shows that I might not have taken all my medicine today, as it's not time.</p>
+                    <p>Each of these lines in the background show me successfully taking my diabetes medication. Yellow is for breakfast, blue is for lunch, and red is for dinner. If there are any black gaps, it shows a time where I forgot to take my meds. If the lines are in the first three lines, odds are it's because it's not time to take my meds yet.</p>
                 </div>
             </div>
         </DefaultLayout>
