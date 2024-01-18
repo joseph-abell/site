a504bdc34ac35361bb9e5c77c1f3211728f2b7eb
Joseph Abell <joseph.a.abell@tescobank.com>
2024-01-18 15:00:11 +0000

    remove password need on priorities

--- src/pages/Priorities.tsx
+++ src/pages/Priorities.tsx
@@ -12,7 +12,6 @@ const Priorities = () => {
   const [data, setData] = createSignal([]);
   const [priorityToEdit, setPriorityToEdit] = createSignal();
   const [password, setPassword] = createSignal('');
-  const [correctPassword, setCorrectPassword] = createSignal(false)
 
   const getData = async () => {
     const { data, error } = await supabase
@@ -50,46 +49,14 @@ const Priorities = () => {
 
   const editPriority = (id?: number) => setPriorityToEdit(id);
   const clearId = () => setPriorityToEdit(undefined);
-  const onPasswordSubmit = async (e: any) => {
-    e.preventDefault();
-
-    const response = await fetch('https://josephabell.co.uk/.netlify/functions/password', {
-        method: 'POST',
-        mode: 'cors',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify({ userInput: password }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
-    console.log(response);
-  }
 
   return (
     <DefaultTemplate>
-        <Show when={correctPassword()}>
-            <div style={{ display: 'flex', 'justify-content': 'space-between'}}>
-                <AddPriority refreshData={getData} id={priorityToEdit} clearId={clearId} />
-                <PrioritiesComponent data={data} />
-                <PriorityList priorities={data} markAsDone={markAsDone} editPriority={editPriority} />
-            </div>
-        </Show>
-
-        <Show when={!correctPassword()}>
-            <h1>Password Locked</h1>
-
-            <form onSubmit={onPasswordSubmit}>
-                <div>
-                    <label style={{display: 'block'}} for='password'>Password</label>
-                    <input type='password' id='password' name='password' onChange={(e) => {setPassword(e.target.value)}} />
-                </div>
-            </form>
-        </Show>
+        <div style={{ display: 'flex', 'justify-content': 'space-between'}}>
+            <AddPriority refreshData={getData} id={priorityToEdit} clearId={clearId} />
+            <PrioritiesComponent data={data} />
+            <PriorityList priorities={data} markAsDone={markAsDone} editPriority={editPriority} />
+        </div>
         
     </DefaultTemplate>
   );
